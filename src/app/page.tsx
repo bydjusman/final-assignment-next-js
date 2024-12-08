76933c767b1bb1c6cad831275294d3ff457fab66
“[firstname <[bydjusman@gmail.com]>
2024-12-08 20:18:34 +0500

    your commit message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Header from "./components/header";
-import Footer from "./components/footer";
-import Herosection from "./components/herosection";
+
 
 export default function Home() {
   return (
    <div>
-    <Header/>
-    <Herosection/>
-    <Footer/>
+   
    </div>
   );
 }
