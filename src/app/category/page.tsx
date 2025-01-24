04c4ebdfaa21da4eb13479548364e3c1b95954dd
ᑌs๓ᴀñ̰︵khokhar <bydjusman@gmail.com>
2025-01-24 19:51:48 +0500

    your commit message

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image"; // Import Next.js Image
+import Image from "next/image"; // Import the Image component
 import Link from "next/link";
 
 const cars = [
